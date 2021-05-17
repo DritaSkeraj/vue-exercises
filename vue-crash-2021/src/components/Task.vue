133d9f165b198e57b7273c1d5c0ca0ae458b1148
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-05-17 02:04:13 +0200

    adds task-tracker project until 56:20

---
+++ vue-crash-2021/src/components/Task.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+  >
+    <h3>
+      {{ task.text }}
+      <i class="fas fa-times" @click="$emit('delete-task', task.id)"></i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style scoped>
+.fas {
+  color: red;
+}
+
+div {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
